5b874fc887b868edb7d59b1a382b296be6d3f8ca
Riddhiman-2007 <77004295+Riddhiman-2007@users.noreply.github.com>
2021-06-09 17:13:14 +0530

    Add files via upload

---
+++ js/Code.js
@@ -0,0 +1,26 @@
+const accessCode1 = "VIRAT KOHLI";
+const accessCode2 = "TIGER SHROFF";
+const accessCode3 = "DISHA PATANI";
+
+function clues() {
+    
+    fill("white")
+    textSize(15)
+    text("T I V R A K H O I L", 100,50)
+    fill("lightblue")
+    text("Hint: The player who has the most number of centuries in IPL!!(give space between name and surname in every question)", 100,70)
+
+// add code for displaying rest of the names and the hints.
+    fill("white")
+    textSize(15)
+    text("R E I G T F O F H S R", 670,150)
+    fill("lightblue")
+    text("Hint: The hero whose last movie was Baaghi 3 !!", 670,170)
+
+    fill("white")
+    textSize(15)
+    text("A D H S I A T A P I N", 100,360)
+    fill("lightblue")
+    text("Hint: The heroin who will be in Ek Villian 3 with John Abraham !!(give space between name and surname in every question)", 100,380)
+
+}
